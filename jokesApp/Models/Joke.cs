7ff913b4ac8c6bc725724a61cabc5116c1516102
Cecilia <cecilia.vinita@gmail.com>
2021-06-10 19:24:37 +1000

    Add project files.

---
+++ jokesApp/Models/Joke.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace jokesApp.Models
+{
+    public class Joke
+    {
+        public int id { get; set; }
+        public string jokeQuestion { get; set; }
+        public string jokeAnswer { get; set; }
+        public Joke()
+        {
+
+        }
+    }
+}
